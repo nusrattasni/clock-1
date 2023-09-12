55e4756b9fb232f866135153924614eff74f3e7a
nusrattasni <136508927+nusrattasni@users.noreply.github.com>
2023-09-11 22:35:36 -1200

    Update clock.c

--- clock.c
+++ clock.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <windows.h>
 
 int main() {
     int h, m, s, n, count;
     count = n = 0;
+
+    int timeDifferences[] = {-10, 5, 4, 2, -10}; // USA, UK, Australia, Dubai, Canada
+
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
     while (count == 0) {
+        system("cls");
         printf("Enter hour : \n");
         scanf("%d", &h);
         printf("Enter minutes : \n");
@@ -17,9 +24,28 @@ int main() {
             system("cls");
         }
     }
+
     while (n == 0) {
         system("cls");
-        printf("%d: %d: %d\n", h, m, s);
+        SetConsoleTextAttribute(hConsole, 7);
+        printf("Local Time: %02d:%02d:%02d\n", h, m, s);
+
+        printf("\t\t\t\n\n\n -------------------------- 5 Country Time table --------------------------\n\n");
+
+        for (int i = 0; i < 5; i++) {
+            int adjustedHour = h + timeDifferences[i];
+            if (adjustedHour < 0) {
+                adjustedHour += 24;
+            } else if (adjustedHour >= 24) {
+                adjustedHour -= 24;
+            }
+            SetConsoleTextAttribute(hConsole, i == 0 ? 12 : i == 1 ? 10 : i == 2 ? 14 : i == 3 ? 9 : 13);
+            printf("%s Time: %02d:%02d:%02d\n",
+                i == 0 ? "\t\tUSA" : i == 1 ? "\t\tUK" : i == 2 ? "\t\tAustralia" : i == 3 ? "\t\tDubai" : "\t\tCanada",
+                adjustedHour, m, s);
+        }
+
+        SetConsoleTextAttribute(hConsole, 7);
         Sleep(1000);
         s++;
         if (s > 59) {
@@ -34,6 +60,7 @@ int main() {
             h = 0;
         }
     }
+
     return 0;
 }
 
